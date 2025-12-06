2299d2f2e6b2227782f7939de95ff77710fa9aef
陸采琪 <tsai@lucaiqideMacBook-Air.local>
2025-12-06 18:13:41 +0800

    update templates and routes

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import api_login, api_logout, AccountListView, PayeeListView, transfer_to_payee, index, TransactionListView
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),  # 這裡對應 index.html
-    path('api/login/', api_login),
-    path('api/logout/', api_logout),
-    path('api/accounts/', AccountListView.as_view()),
-    path('api/payees/', PayeeListView.as_view()),
-    path('api/transfer/', transfer_to_payee),
-    path('api/transactions/', TransactionListView.as_view()),
+    path('login/', views.api_login),
+    path('logout/', views.api_logout),
+
+    path('accounts/', views.AccountListView.as_view()),
+    path('payees/', views.PayeeListView.as_view()),
+    path('transfer/', views.transfer_to_payee),
+    path('transactions/', views.TransactionListView.as_view()),
 ]
